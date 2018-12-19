3d4e6fad892514bd980384129e96729d83535749
plankobostjan <bostjan.planko24@gmail.com>
2018-12-19 19:04:10 +0100

    temp sensor added, garage.py updated

--- garage.py
+++ garage.py
@@ -14,10 +14,27 @@ LED = 17
 
 car_status_changed_wait = 300
 
+def read_temp_raw():
+    f = open(device_file, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
+
+def read_temp():
+    lines = read_temp_raw()
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        lines = read_temp_raw()
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos+2:]
+        temp_c = float(temp_string) / 1000.0
+        temp_f = temp_c * 9.0 / 5.0 + 32.0
+        return temp_c, temp_f
+
 def blink(LED, time):
     distance = checkCar()
     count = 0
-    GPIO.setup(LED, GPIO.OUT)   # Set LedPin's mode is output
     GPIO.output(LED, GPIO.HIGH) # Set LedPin high(+3.3V) to turn on led
     for count in range(0, time):
         GPIO.output(LED, GPIO.HIGH)  # led on
@@ -34,8 +51,6 @@ def blink(LED, time):
     GPIO.cleanup()
 
 def toggleGarage():
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(RELAY, GPIO.OUT)
     GPIO.output(RELAY, 0)
     time.sleep(.5)
     GPIO.output(RELAY, 1)
@@ -51,11 +66,6 @@ def toggleGarage():
         blink(17, 60)
 
 def checkCar():
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(TRIG,GPIO.OUT)
-    GPIO.setup(ECHO,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
-    GPIO.setup(OVERRIDE,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
-
     GPIO.output(TRIG, False)
     time.sleep(2)
 
@@ -72,7 +82,7 @@ def checkCar():
     pulse_duration = pulse_end - pulse_start
     distance = pulse_duration * 17150
 
-    GPIO.cleanup()
+    destroy()
     return round(distance, 2)
 
 def arguments():
@@ -93,6 +103,15 @@ def destroy():
     GPIO.output(LED, GPIO.LOW)   # led off
     GPIO.output(RELAY,GPIO.LOW)
     GPIO.cleanup()
+
+def setup():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(TRIG,GPIO.OUT)
+    GPIO.setup(ECHO,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(OVERRIDE,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(LED, GPIO.OUT)   # Set LedPin's mode is output
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(RELAY, GPIO.OUT)
 
 if __name__=="__main__":
     try:
